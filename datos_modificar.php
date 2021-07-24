c8f9545cb25682e3d673567a4a12eaef5c5920d7
ReyesDavila <87891319+ReyesDavila@users.noreply.github.com>
2021-07-24 02:48:05 -0400

    Create datos_modificar.php

---
+++ datos_modificar.php
@@ -0,0 +1,10 @@
+<?php
+
+//Datos a modificar:
+$usuario="Dixon123";//Usuario dueño del score.
+$clave="123456";//Clave del dueño del score.
+$archivo_ini="savedata.ini";//Nombre del archivo ini
+$archivo_ini_default="savedata_default.ini";//Nombre del archivo ini DEFAULT
+$cantidad_elementos=10;//cantidad de scores a visualizar.
+
+?>
